6ade99db6147028bb932e53fef7fcde5379f33c4
zhenbinwu <zhenbin.wu@gmail.com>
2019-10-30 21:16:35 -0500

    Update Puppi Model

--- PuppiJetMETHTModel.py
+++ PuppiJetMETHTModel.py
@@ -3,18 +3,19 @@ from Common import *
 PhysicsObt = OrderedDict(
     ## Name : [number of object, scale]
     {
-        "puppiJetEt"  : [5, 40],
         "puppiHT"     : [1, 40],
         "puppiMETEt"  : [1, 40],
+        "puppiMETPhi" : [1, 5],
+        "nPuppiJets"  : [1, 0],
+        "puppiJetEt"  : [5, 40],
         "puppiJetEta" : [5, 5],
         "puppiJetPhi" : [5, 5],
-        "puppiMETPhi" : [1, 5],
     }
 )
 
-globalcutfunc = lambda t : (t["puppiJetEt"] > 10).sum()>2
+# globalcutfunc = lambda t : (t["puppiJetEt"] > 10).sum()>2
 
-modelname = "PuppiJetMETHT_cut"
+modelname = "PuppiJetMETHT_NJet_Deep_Reorg"
 
 class autoencoder(nn.Module):
     def __init__(self,features):
@@ -22,24 +23,32 @@ class autoencoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Linear(features, int(features*2/3)),#144,1280
             nn.ReLU(True),
+            nn.Dropout(0.2),
             nn.Linear(int(features*2/3), int(features/3)), # 1280,64
             nn.ReLU(True),
+            nn.Dropout(0.2),
             nn.Linear(int(features/3), int(features/3)), # 1280,64
             nn.ReLU(True),
-            # nn.Linear(int(features/3), int(features/3)), # 1280,64
-            # nn.ReLU(True),
-            # nn.Linear(int(features/3), int(features/3)), # 1280,64
-            # nn.ReLU(True))
+            nn.Dropout(0.2),
+			nn.Linear(int(features/3), int(features/3)), # 1280,64
+			nn.ReLU(True),
+            nn.Dropout(0.2),
+			nn.Linear(int(features/3), int(features/3)), # 1280,64
+			nn.ReLU(True)
         )
         self.decoder = nn.Sequential(
-            # nn.Linear(int(features/3), int(features/3)), # 1280,64
-            # nn.ReLU(True),
-            # nn.Linear(int(features/3), int(features/3)), # 1280,64
-            # nn.ReLU(True),
+			nn.Linear(int(features/3), int(features/3)), # 1280,64
+			nn.ReLU(True),
+            nn.Dropout(0.2),
+			nn.Linear(int(features/3), int(features/3)), # 1280,64
+			nn.ReLU(True),
+            nn.Dropout(0.2),
             nn.Linear(int(features/3), int(features/3)),
             nn.ReLU(True),
+            nn.Dropout(0.2),
             nn.Linear(int(features/3), int(features*2/3)),
             nn.ReLU(True),
+            nn.Dropout(0.2),
             nn.Linear(int(features*2/3), features), 
             nn.ReLU(True))
 
